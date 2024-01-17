e8ef33462624db6eebf2ad691410d591093770b5
hxriif <zaidbnuarifc@gmail.com>
2024-01-17 16:00:37 +0530

    UI-changes

--- front-end-proj/src/Products/Productcategory.jsx
+++ front-end-proj/src/Products/Productcategory.jsx
@@ -21,15 +21,15 @@ export default function Productcategory() {
 
       <h1 className="mt-5" style={{textAlign:"center" ,color:"black	"}}>ꜱʜᴏᴘ ʙʏ ᴄᴀᴛᴇɢᴏʀʏ</h1>
     
-      <MDBCardGroup style={{backgroundColor:'black'}}>
-        <MDBCard  className="mx-2 mt-4 my-4 " onClick={() => Nav("/food")}>
+      <MDBCardGroup >
+        <MDBCard  style={{backgroundColor:'black'}} className="mx-2 mt-4 my-4 " onClick={() => Nav("/food")}>
           <MDBCardImage
             src="https://m.media-amazon.com/images/I/71AHH4xyg8L._SX679_.jpg"
             alt="..."
             position="top"
           />
           <MDBCardBody>
-            <MDBCardTitle>ʙᴀʙʏ ꜰᴏᴏᴅꜱ</MDBCardTitle>
+            <MDBCardTitle className="ms-5" style={{color:"grey"}}>ʙᴀʙʏ ꜰᴏᴏᴅꜱ</MDBCardTitle>
             <MDBCardText>
              
             </MDBCardText>
@@ -39,14 +39,14 @@ export default function Productcategory() {
           </MDBCardBody>
         </MDBCard>
 
-        <MDBCard className="mx-2 mt-4 my-4 " onClick={() => Nav("/Toy")}>
+        <MDBCard  style={{backgroundColor:'black'}} className="mx-2 mt-4 my-4 " onClick={() => Nav("/Toy")}>
           <MDBCardImage
             src="https://m.media-amazon.com/images/I/81Bri2LIWtL._SX679_.jpg"
             alt="..."
             position="top"
           />
           <MDBCardBody>
-            <MDBCardTitle>ʙᴀʙʏ ᴛᴏʏꜱ</MDBCardTitle>
+            <MDBCardTitle  className="ms-5" style={{color:"grey"}}>ʙᴀʙʏ ᴛᴏʏꜱ</MDBCardTitle>
             <MDBCardText>
             
             </MDBCardText>
@@ -56,14 +56,14 @@ export default function Productcategory() {
           </MDBCardBody>
         </MDBCard>
 
-        <MDBCard className="mx-2 mt-4 my-4 " onClick={() => Nav("/Clothe")}>
+        <MDBCard  style={{backgroundColor:'black'}} className="mx-2 mt-4 my-4 " onClick={() => Nav("/Clothe")}>
           <MDBCardImage
             src="https://m.media-amazon.com/images/I/71LxUcbo05L._SX679_PIbundle-3,TopRight,0,0_AA679SH20_.jpg"
             alt="..."
             position="top"
           />
           <MDBCardBody>
-            <MDBCardTitle>ʙᴀʙʏ ᴄʟᴏᴛʜᴇꜱ</MDBCardTitle>
+            <MDBCardTitle  className="ms-5" style={{color:"grey"}}>ʙᴀʙʏ ᴄʟᴏᴛʜᴇꜱ</MDBCardTitle>
             <MDBCardText>
           
             </MDBCardText>
@@ -73,14 +73,14 @@ export default function Productcategory() {
           </MDBCardBody>
         </MDBCard>
 
-        <MDBCard className="mx-2 mt-4 my-4 " onClick={() => Nav("/care")}>
+        <MDBCard  style={{backgroundColor:'black'}} className="mx-2 mt-4 my-4 " onClick={() => Nav("/care")}>
           <MDBCardImage
             src="https://m.media-amazon.com/images/I/81vZm4LRdJL._SX679_.jpg"
             alt="..."
             position="top"
           />
           <MDBCardBody>
-            <MDBCardTitle>ʙᴀʙʏ ᴄᴀʀᴇꜱ</MDBCardTitle>
+            <MDBCardTitle  className="ms-5" style={{color:"grey"}}>ʙᴀʙʏ ᴄᴀʀᴇꜱ</MDBCardTitle>
             <MDBCardText>
              
             </MDBCardText>
